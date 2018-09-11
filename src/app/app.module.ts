fa6e41b4e8bb79af9cb30542d3e50a0981d3717e
Robert <robertm3lendez@gmail.com>
2018-09-11 16:48:14 -0400

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
+import { CheckOutComponent } from './components/check-out/check-out.component';
+import { OrderSuccessComponent } from './components/order-success/order-success.component';
+import { LoginComponent } from './components/login/login.component';
+import { ProductsComponent } from './components/products/products.component';
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
+import { OrdersComponent } from './admin/orders/orders.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    ShoppingCartComponent,
+    CheckOutComponent,
+    OrderSuccessComponent,
+    LoginComponent,
+    ProductsComponent,
+    AdminProductsComponent,
+    OrdersComponent
   ],
   imports: [
     BrowserModule
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+      apiKey: 'AIzaSyDgKIjthz1nDb6cMuqs6qdIGHlB85mTH88',
+      authDomain: 'antojitoschef-9cb63.firebaseapp.com',
+      databaseURL: 'https://antojitoschef-9cb63.firebaseio.com',
+      projectId: 'antojitoschef-9cb63',
+      storageBucket: 'antojitoschef-9cb63.appspot.com',
+      messagingSenderId: '10453201072'
+
+  }
 };
 
 /*
